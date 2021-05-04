a8010726cf94cfd3a4c9b6b5776b1c57b05b2c86
Krissm98 <kriss.m985@gmail.com>
2021-05-04 21:16:39 +0200

    Aufgabe_7_HTML_CSS

---
+++ Aufgabe_7/script_7.js
@@ -0,0 +1,7 @@
+var Aufgabe_7;
+(function (Aufgabe_7) {
+    window.addEventListener("load", function () {
+        document.body.style.backgroundColor = "black";
+    });
+})(Aufgabe_7 || (Aufgabe_7 = {}));
+//# sourceMappingURL=script_7.js.map
