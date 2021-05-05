480dc06a92136d98b5c4ef5144d244cc1c71b0e0
Krissm98 <kriss.m985@gmail.com>
2021-05-05 16:30:50 +0200

    Aufgabe_7_mit_Sounds

--- Aufgabe_7/script_7.js
+++ Aufgabe_7/script_7.js
@@ -1,6 +1,19 @@
 var Aufgabe_7;
 (function (Aufgabe_7) {
     window.addEventListener("load", function () {
+        document.querySelector(".button1").addEventListener("click", function () { playMp3("assets/kick.mp3"); });
+        document.querySelector(".button2").addEventListener("click", function () { playMp3("assets/hihat.mp3"); });
+        document.querySelector(".button3").addEventListener("click", function () { playMp3("assets/snare.mp3"); });
+        document.querySelector(".button4").addEventListener("click", function () { playMp3("assets/A.mp3"); });
+        document.querySelector(".button5").addEventListener("click", function () { playMp3("assets/C.mp3"); });
+        document.querySelector(".button6").addEventListener("click", function () { playMp3("assets/F.mp3"); });
+        document.querySelector(".button7").addEventListener("click", function () { playMp3("assets/G.mp3"); });
+        document.querySelector(".button8").addEventListener("click", function () { playMp3("assets/laugh-1.mp3"); });
+        document.querySelector(".button9").addEventListener("click", function () { playMp3("assets/laugh-2.mp3"); });
+        function playMp3(mp3_Sample) {
+            var sound = new Audio(mp3_Sample);
+            sound.play();
+        }
         document.body.style.backgroundColor = "black";
     });
 })(Aufgabe_7 || (Aufgabe_7 = {}));
--- Aufgabe_7/script_7.ts
+++ Aufgabe_7/script_7.ts
@@ -4,6 +4,27 @@ namespace Aufgabe_7{
 
     window.addEventListener("load", function(){
 
+        
+        document.querySelector (".button1").addEventListener("click", function() { playMp3("assets/kick.mp3")});
+        document.querySelector (".button2").addEventListener("click", function() { playMp3("assets/hihat.mp3")});
+        document.querySelector (".button3").addEventListener("click", function() { playMp3("assets/snare.mp3")});
+        document.querySelector (".button4").addEventListener("click", function() { playMp3("assets/A.mp3")});
+        document.querySelector (".button5").addEventListener("click", function() { playMp3("assets/C.mp3")});
+        document.querySelector (".button6").addEventListener("click", function() { playMp3("assets/F.mp3")});
+        document.querySelector (".button7").addEventListener("click", function() { playMp3("assets/G.mp3")});
+        document.querySelector (".button8").addEventListener("click", function() { playMp3("assets/laugh-1.mp3")});
+        document.querySelector (".button9").addEventListener("click", function() { playMp3("assets/laugh-2.mp3")});
+
+        
+        function playMp3(mp3_Sample){
+            var sound:HTMLAudioElement = new Audio(mp3_Sample); 
+        
+        sound.play();
+
+    }
+
+
+
 
         document.body.style.backgroundColor = "black";
 
