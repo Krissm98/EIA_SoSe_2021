21924f4e8f65f089cb4373837b66842b63489a38
Krissm98 <kriss.m985@gmail.com>
2021-05-21 15:49:01 +0200

    Aufgabe_9_typescrip_fertig

--- Aufgabe_9/Aufgabe_9_script.js
+++ Aufgabe_9/Aufgabe_9_script.js
@@ -11,18 +11,52 @@ var Aufgabe_9;
             toDoArray.push(toDoItem);
             zaehler = zaehler + 1;
             document.querySelector("#zaehlerNr").innerHTML = zaehler.toString();
-            for (var i = 0; i < toDoArray.length; i++) {
-                if (i == 0) {
-                    var clear = document.getElementById("toDoListItem");
-                    clear.innerHTML = "";
-                }
+            var clear = document.getElementById("toDoListItem");
+            clear.innerHTML = "";
+            var _loop_1 = function (i) {
                 var toDoListItem = document.getElementById("toDoListItem");
                 var div = document.createElement("div");
                 div.textContent = toDoArray[i].text;
                 div.classList.add("newToDo");
+                var trashcan = document.createElement("i");
+                trashcan.classList.add("far", "fa-trash-alt");
+                trashcan.addEventListener("click", function () {
+                    div.remove();
+                    toDoArray = toDoArray.filter(function (value) {
+                        return value.text != toDoArray[i].text;
+                    });
+                    zaehler = zaehler - 1;
+                    document.querySelector("#zaehlerNr").innerHTML = zaehler.toString();
+                });
+                var circle = document.createElement("i");
+                circle.classList.add("far", "fa-circle");
+                var circleChecked = document.createElement("i");
+                circleChecked.classList.add("far", "fa-check-circle");
+                if (toDoArray[i].checkmark == false) {
+                    circleChecked.classList.add("isHidden");
+                }
+                else {
+                    circle.classList.add("isHidden");
+                }
+                circle.addEventListener("click", function () {
+                    circle.classList.add("isHidden");
+                    circleChecked.classList.remove("isHidden");
+                    toDoArray[i].checkmark = true;
+                });
+                circleChecked.addEventListener("click", function () {
+                    circleChecked.classList.add("isHidden");
+                    circle.classList.remove("isHidden");
+                    toDoArray[i].checkmark = false;
+                });
                 toDoListItem === null || toDoListItem === void 0 ? void 0 : toDoListItem.appendChild(div);
+                div.appendChild(trashcan);
+                div.appendChild(circle);
+                div.appendChild(circleChecked);
+            };
+            for (var i = 0; i < toDoArray.length; i++) {
+                _loop_1(i);
             }
-            console.log(zaehler);
+            console.log(toDoArray);
         });
     });
 })(Aufgabe_9 || (Aufgabe_9 = {}));
--- Aufgabe_9/Aufgabe_9_script.ts
+++ Aufgabe_9/Aufgabe_9_script.ts
@@ -14,9 +14,14 @@ namespace Aufgabe_9 {
 
 
         ];
-      
+
 
         document.querySelector(".enter").addEventListener("click", function (): void {
+
+
+
+
+
 
             let toDoItem: ToDoBlocks = {
                 text: (<HTMLInputElement>document.getElementById("textFeldTd")).value,
@@ -26,23 +31,76 @@ namespace Aufgabe_9 {
             toDoArray.push(toDoItem);
             zaehler = zaehler + 1;
             document.querySelector("#zaehlerNr").innerHTML = zaehler.toString();
-        
-         
+
+            const clear = document.getElementById("toDoListItem");
+            clear.innerHTML = ``;
+
             for (let i: number = 0; i < toDoArray.length; i++) {
 
 
-                if (i == 0) {
-                    const clear = document.getElementById("toDoListItem");
-                    clear.innerHTML = ``;
-                }
+
+
 
                 const toDoListItem: HTMLElement = document.getElementById("toDoListItem");
-                const div: HTMLElement = document.createElement("div");
+                const div: HTMLDivElement = document.createElement("div");
                 div.textContent = toDoArray[i].text;
                 div.classList.add("newToDo");
-                toDoListItem?.appendChild(div);
 
-        
+
+
+                const trashcan: HTMLElement = document.createElement("i");
+                trashcan.classList.add("far", "fa-trash-alt");
+
+                trashcan.addEventListener("click", function (): void {
+                    div.remove();
+                    toDoArray = toDoArray.filter(function (value: ToDoBlocks): boolean {
+                        return value.text != toDoArray[i].text;
+
+                    });
+
+                    zaehler = zaehler - 1;
+                    document.querySelector("#zaehlerNr").innerHTML = zaehler.toString();
+
+                });
+
+
+
+
+                const circle: HTMLElement = document.createElement("i");
+                circle.classList.add("far", "fa-circle");
+
+
+
+                const circleChecked: HTMLElement = document.createElement("i");
+                circleChecked.classList.add("far", "fa-check-circle");
+
+                if (toDoArray[i].checkmark == false) { 
+                    circleChecked.classList.add("isHidden"); 
+                }
+                else { 
+                    circle.classList.add("isHidden"); 
+                }
+
+                circle.addEventListener("click", function (): void {
+                    circle.classList.add("isHidden");
+                    circleChecked.classList.remove("isHidden");
+                    toDoArray[i].checkmark = true;
+                });
+                circleChecked.addEventListener("click", function (): void {
+                    circleChecked.classList.add("isHidden");
+                    circle.classList.remove("isHidden");
+                    toDoArray[i].checkmark = false;
+                    });
+
+
+
+                toDoListItem?.appendChild(div);
+                div.appendChild(trashcan);
+                div.appendChild(circle);
+                div.appendChild(circleChecked);
+
+
+
 
 
 
@@ -63,11 +121,11 @@ namespace Aufgabe_9 {
 
 
             }
-            console.log(zaehler);
+            console.log(toDoArray);
 
-        
 
-         
+
+
 
 
 
